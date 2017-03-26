299fc4ae6e4f0433091f872de180fd4c76365d92
eloygil <eloy.gil@est.fib.upc.edu>
2017-03-26 23:17:40 +0200

    4th Lab - In progress...

---
+++ Lab04/geo_data_gen.py
@@ -0,0 +1,23 @@
+import json
+
+fname = '../Lab03/BarcelonaTweets.json'
+with open(fname, 'r') as f:
+    geo_data = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for line in f:
+        tweet = json.loads(line)
+        if tweet['coordinates']:
+            geo_json_feature = {
+                "type": "Feature",
+                "geometry": tweet['coordinates'],
+                "properties": {
+                    "text": tweet['text'],
+                    "created_at": tweet['created_at']
+                }
+            }
+            geo_data['features'].append(geo_json_feature)
+ 
+with open('geo_data.json', 'w') as fout:
+    fout.write(json.dumps(geo_data, indent=4))
